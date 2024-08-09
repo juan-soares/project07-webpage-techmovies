be8984b9b42a2bb6cd45f734f653209a77724cb3
juan-soares <juan.css013@gmail.com>
2024-08-09 16:19:06 -0300

    refactor: encapsulate responsibilities in function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,18 +2,28 @@ const btnList = document.querySelectorAll("button");
 const imgCoverList = document.querySelectorAll(".cover");
 const divSynopses = document.querySelectorAll(".synopses");
 
+const updateActiveCoverImage = (index) => {
+  const imgCoverActive = document.querySelector(".cover--active");
+  imgCoverActive.classList.remove("cover--active");
+  imgCoverList[index].classList.add("cover--active");
+};
+
+const updateActiveSynopses = (index) => {
+  const divSynopsesActive = document.querySelector(".synopses--active");
+  divSynopsesActive.classList.remove("synopses--active");
+  divSynopses[index].classList.add("synopses--active");
+};
+
+const updateActiveButton = (button) => {
+  const btnActive = document.querySelector(".btn--active");
+  btnActive.classList.remove("btn--active");
+  button.classList.add("btn--active");
+};
+
 btnList.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    const imgCoverActive = document.querySelector(".cover--active");
-    imgCoverActive.classList.remove("cover--active");
-    imgCoverList[index].classList.add("cover--active");
-
-    const divSynopsesActive = document.querySelector(".synopses--active");
-    divSynopsesActive.classList.remove("synopses--active");
-    divSynopses[index].classList.add("synopses--active");
-
-    const btnActive = document.querySelector(".btn--active");
-    btnActive.classList.remove("btn--active");
-    btn.classList.add("btn--active");
+    updateCoverImage(index);
+    updateSynopses(index);
+    updateActiveButton(btn);
   });
 });
