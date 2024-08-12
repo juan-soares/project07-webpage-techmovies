3a49fd18c5aa00f18a10d33c5760500b1382225f
juan-soares <juan.css013@gmail.com>
2024-08-12 09:13:45 -0300

    refactor: converted functions into generic.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,17 @@
-const btnList = document.querySelectorAll("button");
 const imgCoverList = document.querySelectorAll(".cover");
 const divSynopses = document.querySelectorAll(".synopses");
+const btnList = document.querySelectorAll("button");
 
-const updateActiveCoverImage = (index) => {
-  const imgCoverActive = document.querySelector(".cover--active");
-  imgCoverActive.classList.remove("cover--active");
-  imgCoverList[index].classList.add("cover--active");
-};
-
-const updateActiveSynopses = (index) => {
-  const divSynopsesActive = document.querySelector(".synopses--active");
-  divSynopsesActive.classList.remove("synopses--active");
-  divSynopses[index].classList.add("synopses--active");
-};
-
-const updateActiveButton = (button) => {
-  const btnActive = document.querySelector(".btn--active");
-  btnActive.classList.remove("btn--active");
-  button.classList.add("btn--active");
+const updateActiveElement = (elementClass, elementList, elementIndex) => {
+  const activeElement = document.querySelector(`.${elementClass}--active`);
+  activeElement.classList.remove(`${elementClass}--active`);
+  elementList[elementIndex].classList.add(`${elementClass}--active`);
 };
 
 btnList.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    updateActiveCoverImage(index);
-    updateActiveSynopses(index);
-    updateActiveButton(btn);
+    updateActiveElement("cover", imgCoverList, index);
+    updateActiveElement("synopses", divSynopses, index);
+    updateActiveElement("btn", btnList, index);
   });
 });
